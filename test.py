94f0252c9bf09d43365037e49b6a0cceef51357e
Mohit Kumhar <mohitmolela@gmail.com>
2025-07-10 12:52:03 +0530

    liniting test file

--- test.py
+++ test.py
@@ -7,8 +7,8 @@ from unittest.mock import patch
 import pytest
 from app import app
 
-@pytest.fixture
-def client():
+@pytest.fixture(name='client')
+def client_fixture():
     """
     Enabling Flask Testing Mode by Turning of error catching 
     and returning test client that can make HTTP requests to the app
@@ -18,21 +18,21 @@ def client():
 
 
 # Test 1: Check if login page loads correctly
-def test_login_page_loads(login_client):
+def test_login_page_loads(client):
     """
     Test if the login page loads correctly
     """
-    response = login_client.get('/')
+    response = client.get('/')
     assert response.status_code == 200
 
 # Test 2:  Simulate login with invalid user
-def test_invalid_user_login(invalid_client):
+def test_invalid_user_login(client):
     """
     Test by log in with invalid username
     """
 
     with patch('app.users_collection.find_one', return_value=None):
-        response = invalid_client.post("/", data={'username': "wronguser"}, follow_redirects=True)
+        response = client.post("/", data={'username': "wronguser"}, follow_redirects=True)
 
         assert response.status_code == 200
         assert b'invalid username not found' in response.data.lower()
